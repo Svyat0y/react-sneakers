091356db0c3f0ebd59780535850e0c2fc0cd41c6
shurik <86realmadrid86@gmail.com>
2022-05-03 02:54:17 +0300

    refactoring drawer
    order button moved inside div element

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -38,53 +38,57 @@ const Drawer = (): JSX.Element => {
 				<div className={ styles.items }>
 
 					{
-						!isLoading
-							? <>
-								{ cartItems.length > 0
-									? cartItems && cartItems.map(item =>
-									<div key={ item.id } className={ styles.cartItem }>
-										<img width={ 70 } height={ 70 } src={ item.img } alt='sneakers'/>
-										<div>
-											<p>{ item.title }</p>
-											<b>{ item.price } руб.</b>
-										</div>
-										<img
-											onClick={ () => onRemoveCart(item) }
-											width={ 32 } height={ 32 }
-											className={ styles.removeBtn }
-											src='/img/remove_btn.svg'
-											alt='close'/>
-									</div>)
-									: <Info
-										title={ 'Корзина пустая' }
-										description={ 'Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.' }
-										onClose={ closeCart }
-										size={ 120 }
-										image='/img/empty_cart.svg'
-									/>
+						isLoading
+							? <Spinner/>
+							: <>
+								<div style={ { flex: '1 0 auto' } }>
+									{
+										cartItems.length > 0
+											? cartItems && cartItems.map(item =>
+											<div key={ item.id } className={ styles.cartItem }>
+												<img width={ 70 } height={ 70 } src={ item.img } alt='sneakers'/>
+												<div>
+													<p>{ item.title }</p>
+													<b>{ item.price } руб.</b>
+												</div>
+												<img
+													onClick={ () => onRemoveCart(item) }
+													width={ 32 } height={ 32 }
+													className={ styles.removeBtn }
+													src='/img/remove_btn.svg'
+													alt='close'/>
+											</div>)
+											: <Info
+												title={ 'Корзина пустая' }
+												description={ 'Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.' }
+												onClose={ closeCart }
+												size={ 120 }
+												image='/img/empty_cart.svg'
+											/>
+									}
+								</div>
+								{
+									cartItems.length > 0 && <div className={ styles.cartTotalBlock }>
+										<ul>
+											<li className='d-flex'>
+												<span>Итого:</span>
+												<div></div>
+												<b>21 498 руб.</b>
+											</li>
+											<li className='d-flex'>
+												<span>Налог 5%</span>
+												<div></div>
+												<b>1074 руб.</b>
+											</li>
+										</ul>
+										<button className={ `${ styles.greenButton } greenButton` }>
+											Оформить заказ
+											<img src='/img/arrow.svg' alt='orderArrow'/>
+										</button>
+									</div>
 								}
 							</>
-							: <Spinner/>
 					}
-
-				</div>
-				<div className={ styles.cartTotalBlock }>
-					<ul>
-						<li className='d-flex'>
-							<span>Итого:</span>
-							<div></div>
-							<b>21 498 руб.</b>
-						</li>
-						<li className='d-flex'>
-							<span>Налог 5%</span>
-							<div></div>
-							<b>1074 руб.</b>
-						</li>
-					</ul>
-					<button className={ `${ styles.greenButton } greenButton` }>
-						Оформить заказ
-						<img src='/img/arrow.svg' alt='orderArrow'/>
-					</button>
 				</div>
 			</div>
 		</div>
